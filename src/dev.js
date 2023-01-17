aa822b7d4d569b771b51961090328ea77600d04f
Eric Hart <>
2023-01-17 15:32:02 +0800

    [Add]

--- src/dev.js
+++ src/dev.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '~/style/index.css';
-import RegionStaticList from '~/script/component/RegionStaticList';
+import Tab from '~/script/component/Tab';
 
 const data = [
   'CSS transitions provide a way to control animation speed when changing CSS properties. Instead of having property changes take effect immediately, you can cause the changes in a property to take place over a period of time. For example, if you change the color of an element from white to black, usually the change is instantaneous. With CSS transitions enabled, changes occur at time intervals that follow an acceleration curve, all of which can be customized.1',
@@ -15,4 +15,4 @@ const data = [
 ];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RegionStaticList data={data} />);
+root.render(<Tab />);
--- src/script/component/Dropdown/index.js
+++ src/script/component/Dropdown/index.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import style from './index.module.css';
 
+function formatUnit(val) {
+  return val + 'px';
+}
+
 class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.id = new Date().getTime().toString();
+    this.state = {
+      data: ['instance4', 'instance5', 'instance6'],
+      height: undefined,
+      open: true,
+    };
+    this.transition = this.transition.bind(this);
+  }
+
+  componentDidMount() {
+    const { id, } = this;
+    const dom = document.getElementById(id);
+    this.dom = dom;
+    this.close();
+  }
+
+  transition() {
+    let { height, } = this.state;
+    const { dom, } = this;
+    if (height === undefined) {
+      height = dom.offsetHeight;
+    }
+    const newHeight = height - 4;
+    this.setState({
+      height: newHeight,
+    });
+    if (newHeight > 0) {
+      window.requestAnimationFrame(this.transition);
+    } else {
+      this.setState({
+        open: false,
+      });
+    }
+  }
+
+  close() {
+    this.transition();
+  }
+
   render() {
-    const data = ['instance4', 'instance5', 'instance6'];
-    const list = data.map((e, i) => {
+    const { id, } = this;
+    const { data, height, } = this.state;
+    const items = data.map((e, i) => {
       let item;
       switch (i) {
         case 0:
@@ -19,11 +65,15 @@ class Dropdown extends React.Component {
       }
       return item;
     });
-    return(
-      <ul className={style.dropdown}>
-        {list}
-      </ul>
-    );
+    let list = null;
+    const { open, } = this.state;
+    if (open) {
+      list =
+        <ul id={id} className={style.dropdown} style={{ height: formatUnit(height) }}>
+          {items}
+        </ul>;
+    }
+    return list;
   }
 }
 
