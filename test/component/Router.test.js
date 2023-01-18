7f05b9195a8c44fbaa3cec23ae03b5a918c18acd
Eric Hart <>
2023-01-18 17:57:26 +0800

    [Add]

--- test/component/Router.test.js
+++ test/component/Router.test.js
@@ -1,4 +1,4 @@
-import { renderToStaticMarkup, } from 'react-dom/server';
+import TestRenderer from 'react-test-renderer';
 import { describe, expect, test, } from '@jest/globals';
 import React from 'react';
 import Router from '~/script/component/Router';
@@ -6,6 +6,6 @@ import Router from '~/script/component/Router';
 describe('[component] Router', () => {
   test('router render result should correct.', () => {
     const router = <Router />;
-    expect(renderToStaticMarkup(<Router />)).toBe('ifsadfasd');
+    expect(TestRenderer.create(<Router />).toJSON()).toBe(null);
   });
 });
